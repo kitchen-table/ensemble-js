2597181de2cecd4da00bb349a5b67b610a887bb0
Jonghakseo <unqocn@gmail.com>
2023-09-23 14:29:13 +0900

    fix core, api

--- apps/js-sdk/lib/core.ts
+++ apps/js-sdk/lib/core.ts
@@ -1,12 +1,13 @@
 import EventEmitter from './eventEmitter';
 import Api from './api';
-import { EventType, PointerMoveOutput, type User } from '@packages/api';
+import { EventType, PointerMoveOutput, RoomJoinOutput, type User } from '@packages/api';
 
 class KitchenTable {
   roomId: string;
   api: Api;
   events: EventEmitter;
   myInfo: User | undefined;
+  userList: User[] = [];
 
   private constructor(api: Api) {
     this.api = api;
@@ -23,10 +24,12 @@ class KitchenTable {
   }
 
   private async setup() {
-    const { myInfo } = await this.api.join({ roomId: this.roomId });
+    const { myInfo } = await this.api.login({ roomId: this.roomId });
     this.myInfo = myInfo;
     this.events.bind(window.addEventListener);
     this.bindOnMessage();
+    this.joinRoom();
+    this.setUserList();
 
     // TODO: cleanup or re-init
     window.addEventListener('locationchange', (evt) => {
@@ -34,7 +37,23 @@ class KitchenTable {
     });
   }
 
+  private joinRoom() {
+    this.api.emit(EventType.ROOM_JOIN, { roomId: this.roomId });
+  }
+  private async setUserList() {
+    const { users } = await this.api.getUserList({ roomId: this.roomId });
+    this.userList = users.filter((user) => user.id !== this.myInfo?.id);
+  }
+
   private bindOnMessage() {
+    this.api.listen(EventType.ROOM_JOIN, (data: RoomJoinOutput) => {
+      const joinUserInfo = data.myInfo;
+      if (joinUserInfo.id === this.myInfo?.id) {
+        return;
+      }
+      console.log('join new user!', joinUserInfo);
+    });
+
     this.api.listen(EventType.POINTER_MOVE, (data: PointerMoveOutput) => {
       let isMe = false;
       const cursorId = `kitchen-table-${data.userId}`;
@@ -55,12 +74,14 @@ class KitchenTable {
         cursor.style.top = cursorTop;
       } else {
         const cursor = document.createElement('div');
+        const userColor = this.userList.find((user) => user.id === data.userId)?.color ?? 'red';
+        const bgColor = isMe ? 'blue' : userColor;
         cursor.id = cursorId;
         cursor.style.position = 'absolute';
         cursor.style.width = '10px';
         cursor.style.height = '10px';
         cursor.style.borderRadius = '50%';
-        cursor.style.backgroundColor = isMe ? 'blue' : 'red';
+        cursor.style.backgroundColor = bgColor;
         cursor.style.left = cursorLeft;
         cursor.style.top = cursorTop;
         document.body.appendChild(cursor);
