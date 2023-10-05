b8545e955ca286793371c8587c6dcc9e6623d018
JongHak Seo <unqocn@gmail.com>
2023-10-05 22:52:01 +0900

    add path attr into user

--- packages/api/src/events/update-my-info.ts
+++ packages/api/src/events/update-my-info.ts
@@ -5,6 +5,7 @@ export interface UpdateMyInfoInput {
   name?: User['name'];
   color?: User['color'];
   isBackground?: User['isBackground'];
+  path?: User['path'];
 }
 
 export interface UpdateMyInfoOutput {
