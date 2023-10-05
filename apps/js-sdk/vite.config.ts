c9c9dd78e452c811e62bfb9be1296e439d755626
Jonghakseo <jonghak.seo@creatrip.com>
2023-10-06 02:39:30 +0900

    reduce sdk size

--- apps/js-sdk/vite.config.ts
+++ apps/js-sdk/vite.config.ts
@@ -4,6 +4,7 @@ import packageJson from './package.json';
 import preact from '@preact/preset-vite';
 import svgr from '@svgr/rollup';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import terser from '@rollup/plugin-terser';
 
 const name = packageJson.name;
 
@@ -12,12 +13,24 @@ const isDev = process.env.NODE_ENV === 'development';
 export default defineConfig({
   plugins: [tsconfigPaths(), svgr(), preact()],
   build: {
-    minify: !isDev,
+    minify: !isDev && 'terser',
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
       name,
       fileName: (format) => `${name}.${format}.js`,
     },
+    rollupOptions: isDev
+      ? undefined
+      : {
+          plugins: [
+            terser({
+              format: { comments: false },
+              mangle: {
+                properties: true,
+              },
+            }),
+          ],
+        },
   },
   define: { 'process.env.NODE_ENV': isDev ? '"development"' : '"production"' },
 });
