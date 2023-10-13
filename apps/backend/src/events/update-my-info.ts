05733a711f1b7ade3172998c704d31c333fe99ae
JongHak Seo <unqocn@gmail.com>
2023-10-13 16:51:41 +0900

    add path update logic

--- apps/backend/src/events/update-my-info.ts
+++ apps/backend/src/events/update-my-info.ts
@@ -11,6 +11,7 @@ export function onUpdateMyInfo(io: Server, socket: Socket) {
     if (data.name) user.name = data.name;
     if (data.color) user.color = data.color;
     if (data.isBackground !== undefined) user.isBackground = data.isBackground;
+    if (data.path) user.path = data.path;
 
     const output: UpdateMyInfoOutput = {
       success: true,
