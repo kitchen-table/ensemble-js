362f2bed44bdfe412a9ded0a1741067326aaf0b3
JongHak Seo <unqocn@gmail.com>
2023-10-27 19:45:41 +0900

    feat: add show message toggle button

--- apps/js-sdk/lib/ui/FAB/FABSettings.tsx
+++ apps/js-sdk/lib/ui/FAB/FABSettings.tsx
@@ -3,7 +3,6 @@ import FABIcon from 'ui/FAB/FABIcon';
 import { BiCog } from 'react-icons/bi';
 import { resolve, TYPE } from 'di';
 import styled from 'ui/styled';
-import { ComponentChildren } from 'preact';
 import Switch from 'ui/FAB/Switch';
 
 export default function FABSettings() {
@@ -23,22 +22,43 @@ export default function FABSettings() {
 function UserSetting() {
   const getUiState = resolve(TYPE.UI_STATE_STORAGE);
   const showCursor = getUiState().getShowCursor();
+  const showMessage = getUiState().getShowMessage();
+
   const toggleShowCursor = (checked: boolean) => {
     getUiState().setShowCursor(checked);
   };
 
+  const toggleShowMessage = (checked: boolean) => {
+    getUiState().setShowMessage(checked);
+  };
+
   return (
     <UserSettingContainer>
-      <Switch checked={showCursor} onCheckedChange={toggleShowCursor}>
-        Show Cursor
-      </Switch>
+      <label>
+        <Switch checked={showCursor} onCheckedChange={toggleShowCursor} />
+        <span>Show Cursor</span>
+      </label>
+      <label>
+        <Switch checked={showMessage} onCheckedChange={toggleShowMessage} />
+        <span>Show Message</span>
+      </label>
     </UserSettingContainer>
   );
 }
 
 const UserSettingContainer = styled.div`
-  padding: 12px;
+  width: 200px;
   display: grid;
   grid-template-columns: 1fr;
   gap: 8px;
+
+  > label {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+
+    cursor: pointer;
+    font-size: 14px;
+    user-select: none;
+  }
 `;
